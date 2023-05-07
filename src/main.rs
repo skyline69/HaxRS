1311b3f76e3c7e2b33a8fbb7ab296dff8c1defc9
skyline <efedasguney@protonmail.com>
2023-05-07 18:18:04 +0200

    Worked on Windows compatibility.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 mod behind;
 
 use behind::cli;
+use crossterm::{
+    execute,
+    terminal::SetTitle,
+    Result,
+};
+use std::io::stdout;
 
-fn main() {
-    cli::clear_terminal();
+
+fn main() -> Result<()> {
+    execute!(stdout(), SetTitle("HaxRS"))?;
     cli::print_login_logo();
-    cli::menu_table()
+    cli::menu_table();
+    Ok(())
 }
